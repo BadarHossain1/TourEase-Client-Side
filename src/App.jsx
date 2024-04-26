68550b56ee20bc4ffdbf04658e314c29e5dd3a52
Badar Hossain <badar12041@gmail.com>
2024-04-26 23:07:20 +0600

    The client side setup Completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='w-full h-1/2 bg-[#f0fcf9]'>
+      
+      <h1 className='text-4xl text-[#1ea67c]'>Vite + React</h1>
+      
+    </div>
+  )
+}
+
+export default App
